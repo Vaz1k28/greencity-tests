ad180e061326df193b2a820c575272a262985906
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2026-05-05 19:17:46 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,17 +1,9 @@
 import pytest
 from selenium import webdriver
-from src.pages.events_page import EventsPage
 
-@pytest.fixture
+@pytest.fixture()
 def driver():
-
-    driver = webdriver.Firefox() # Або webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.maximize_window()
     yield driver
     driver.quit()
-
-@pytest.fixture
-def events_page(driver):
-    
-    page = EventsPage(driver)
-    return page
